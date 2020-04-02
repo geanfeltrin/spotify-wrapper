afa7b2a311447c1fb994346970b3823c5753a023
Gean Feltrin <gean_costa@hotmail.com>
2020-04-02 19:48:23 -0300

    config

--- src/main.js
+++
@@ -1,2 +0,0 @@
-console.log('lint');
-const as = 1;
---
+++ tests/main.spec.js
@@ -0,0 +1,8 @@
+import { expect } from 'chai';
+
+
+describe('smoke-teste', () => {
+  it('should exist method `Search`', () => {
+    expect(search).to.exist;
+  });
+});
