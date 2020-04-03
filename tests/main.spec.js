2cb7f0d81c8a5fdc36341483c84583b495b3934b
Gean Feltrin <gean_costa@hotmail.com>
2020-04-03 10:46:38 -0300

    Smoke test

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,8 +1,28 @@
 import { expect } from 'chai';
+import {
+  search,
+  searchAlbums,
+  searchArtists,
+  searchTracks,
+  searchPlaylists,
+} from '../src/main';
 
-
-describe('smoke-teste', () => {
-  it('should exist method `Search`', () => {
-    expect(search).to.exist;
+describe('Spotify Wrapper', () => {
+  describe('smoke-teste', () => {
+    it('should exist the search method', () => {
+      expect(search).to.be.a('function');
+    });
+    it('should exist the searchAlbums method', () => {
+      expect(searchAlbums).to.be.a('function');
+    });
+    it('should exist the searchArtists method', () => {
+      expect(searchArtists).to.be.a('function');
+    });
+    it('should exist the searchTracks method', () => {
+      expect(searchTracks).to.be.a('function');
+    });
+    it('should exist the searchPlaylists method', () => {
+      expect(searchPlaylists).to.be.a('function');
+    });
   });
 });
