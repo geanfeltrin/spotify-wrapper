06163dced671696873c9d8d5c1dfc4b20e2bafaa
Gean Feltrin <gean_costa@hotmail.com>
2020-04-03 12:44:36 -0300

    Create Function search

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
-export const search = () => {
-
-};
+export const search = (query, type) => fetch(`https://api.spotify.com/v1/search?q=${query}&type=${type}`).then((data) => data.json());
 
 export const searchAlbums = () => {
 
