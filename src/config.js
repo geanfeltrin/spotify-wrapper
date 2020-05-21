853ed081e2d3d39040a74432f4039fb667ef60e7
Gean Feltrin <gean_costa@hotmail.com>
2020-05-21 09:51:13 -0300

    Refq config

--- src/config.js
+++ src/config.js
@@ -1,11 +1,5 @@
 
-const TOKEN_API = 'BQBr5Mly6NX4vC0DtYPbuAzC-2JBgX6jtNCbA5AXER7F2nwtM9rvnBNSt5uxy8sBt0w59dS2gTwXHbObzw4i53iMkcxz5MCclH3FLv5QcVguDIHaBFaxa7XnULo9gFHsDb9G71wD_C7zD6evF4P4zrwwZ1ZRYLM';
-
-export const API_URL = 'https://api.spotify.com/v1';
+const API_URL = 'https://api.spotify.com/v1';
 
 
-export const HEADERS = {
-  headers: {
-    Authorization: `Bearer ${TOKEN_API}`,
-  },
-};
+export default API_URL;
